8d7e4f4b7d596fcbb7f4a5ef4d705548ef14bc64
hashinmohammed <hashinpks@gmail.com>
2023-01-05 14:19:12 +0530

    keeper part1

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+
+
+    return(
+        <div className="note" >
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+export default Note;
