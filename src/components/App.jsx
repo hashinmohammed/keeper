41fd30530e21613e40a617c052fdabfc0d19b58b
hashinmohammed <hashinpks@gmail.com>
2023-01-06 18:44:42 +0530

    Keeper Second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+// console.log(notes);
 
+// function createNotes(noteItem){
+//   return <Note
+//   key={noteItem.key}
+//   title={noteItem.title}
+//   content={noteItem.content}
+//   />
+// }
 function App() {
   return (
     <div>
       <Header></Header>
-      <Note></Note>
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer></Footer>
     </div>
   );
